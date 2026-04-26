0a47fea4916197266f9c79bf129f881054e7c42f
Timothy Nzuki <nzukimyles89@gmail.com>
2026-04-26 13:36:39 +0300

    A continuation of JS-Foundations. It's about data types.

---
+++ 2-data-types.js
@@ -0,0 +1,40 @@
+// A data types defines the kind of value will be assigned to a varible and the operations that can be performed on it.
+// Data is clacified into two broad types in JS.
+
+// 1. Primitive Data Types
+// These are the basic.
+
+// They are: String, Number, Boolean, Null, Undefined. 
+// Symbols is part of this, but will talk about it later.
+
+// String.
+// String represents words or a sentence and the main identifyer is that the word or the sentence is rapped inside single speechmark.
+// e.g 'Nzuki'
+// const  name= 'Nzuki';
+// let name= 'Nzuki';
+
+// Number.
+// This represents mathematical values. Both whole numbers and decimals.
+// e.g 20, 3.4
+// const age= 22;
+// let age= 22;
+
+// Boolean.
+// This represent values that are considered either true or false.
+// They are mainly used where a cindition is given.
+// e.g isStudent = true;
+// contst isStudent= true;
+// let isStudent= true;
+// In the writing of boolean the camelCase is most used when deaing with JS. Especially for readability.
+
+// Null.
+// This means the value is empty. It's  usually intentional.
+// e.g. let nothing= null;
+
+// Undefined.
+// Majority of the people confuse between undefined and null. 
+// In undefined, it's declaring the variable without assigning it.
+// With  this knowledge it means that we can never use the variable CONST because a CONST has to be assigned immediatley.
+// e.g. let notAssigned;
+
+
